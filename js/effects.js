d17d539f11d18b3876a83b3e876bc074a514904e
Adrián <adrigibal@gmail.com>
2017-03-13 23:19:24 +0100

    Anayadir jQuery, efectos login, permitir listado directorios

--- js/effects.js
+++ js/effects.js
@@ -1,7 +1,10 @@
-function ready() {
-	button = document.findElementById('boton_empezar');
-	button.click(function() {
-		ventanaLogin = document.findElementById('ventana_login');
-		ventanaLogin.css('display', 'none');
-	});
-}
+$(document).ready(function() {
+	$('#boton_empezar').click(function() {
+		$(this).animate({opacity: 0}, function() {
+			$('#ventana_login').animate({opacity: 0}, function() {
+				// Desactivar visualizacion de la forma debida, por si acaso:
+				$(this).css('display', 'none')
+			})
+		})
+	})
+})
